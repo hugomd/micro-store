d83635c3c819d27e009c9c48963119b28b2e1923
Hugo <h@hugo.sx>
2017-03-01 00:31:58 +1100

    Add core functionality

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const bodyParser = require('body-parser')
+const cache = require('memory-cache')
+const uuid = require('uuid/v4')
+const Package = require('./package')
+
+const TWENTY_FOUR_HOURS = 86400000
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.redirect(Package.homepage)
+})
+
+app.get('/:pasteId', (req, res) => {
+  const data = cache.get(req.params.pasteId)
+  if (!data) return res.send('No paste found.').status(404)
+  res.send(data)
+})
+
+app.put('/:pasteId', (req, res) => {
+  const pasteId = req.params.pasteId
+  cache.put(pasteId, req.body, TWENTY_FOUR_HOURS)
+  res.send({ pasteId })
+})
+
+app.post('/', (req, res) => {
+  const pasteId = uuid()
+  cache.put(pasteId, req.body, TWENTY_FOUR_HOURS)
+  res.send({ pasteId })
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
